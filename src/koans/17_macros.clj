08d84683af93e6fac055420ad9863456e14f7574
Will Funnell <will.funnell@bbc.co.uk>
2014-03-06 00:31:51 +0000

    Finished macros

--- src/koans/17_macros.clj
+++ src/koans/17_macros.clj
@@ -17,7 +17,7 @@
         :else
         (let [operator (second form)
               first-arg (first form)
-              others __]
+              others (rest (rest form))]
           `(~operator
             (r-infix ~first-arg)
             (r-infix ~others)))))
